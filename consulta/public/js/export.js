d8a91137ddeb83d94fdd7ea02d7c110a7c965926
YaoCr003 <a01275893@tec.mx>
2024-04-08 13:22:47 -0600

    Fix

--- consulta/public/js/export.js
+++ consulta/public/js/export.js
@@ -1,12 +1,12 @@
 //Codigo para exportar PDF
 document.getElementById('btnExportPDF').addEventListener('click', async () => {
     const urlActual = window.location.href;
-    const idMatch = urlActual.match(/https?:\/\/([^/]+)\/paciente\/(\d+)/);
-    if (!idMatch || idMatch.length < 3) {
-        console.error('No se pudo extraer el ID del paciente de la URL.');
+    const idMatch = urlActual.match(/https?:\/\/[^/]+\/paciente\/([\w-]+)/);
+    if (!idMatch || idMatch.length < 2) {
+        console.error('No se pudo extraer el ID completo del paciente de la URL.');
         return;
     }
-    const id = idMatch[0].split('/').pop(); // Tomamos el último segmento de la URL
+    const id = idMatch[1];
 
     const response = await fetch('/otroPdf/'+id);
     const blob = await response.blob();
